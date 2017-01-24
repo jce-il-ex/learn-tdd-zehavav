8377ddb6e616629a2e5b70b717597e8d4e1eaa07
zehavav <zlvilman@gmail.com>
2017-01-24 20:24:38 +0200

    RED: Fialing test

--- cash.js
+++ cash.js
@@ -1,4 +1,5 @@
 var C = {};                    // C Object simplifies exporting the module
+var coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
     'use strict';
     return [50, 20, 20];    // just enough to pass :-)
